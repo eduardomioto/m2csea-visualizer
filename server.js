cb30519826279552e94f7ba2c95ee829d9cc4ee3
Eduardo Mioto <32001941+eduardomioto-zoop@users.noreply.github.com>
2019-07-07 23:13:13 -0300

    creating a node server to serve

---
+++ server.js
@@ -0,0 +1,69 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    var filePath = request.url;
+    if (filePath == '/'){
+		filePath = 'index.html';
+	}
+	filePath = './static/' + filePath;
+        
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+	
+	// Website you wish to allow to connect
+	response.setHeader('Access-Control-Allow-Origin', '*');
+
+	// Request methods you wish to allow
+	response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+	// Request headers you wish to allow
+	response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	response.setHeader('Access-Control-Allow-Credentials', true);
+
+    fs.readFile(filePath, function(error, content) {
+		if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(18080);
+console.log('MC2PD Visualizer running at http://127.0.0.1:18080/');
